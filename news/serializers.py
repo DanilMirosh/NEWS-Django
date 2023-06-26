223117ff6cc52579b97af448184b35e96ab2085b
DanilMirosh <miroshnichenko.da.vi@mail.ru>
2023-06-26 22:22:28 +0500

    work with GRUD

--- news/serializers.py
+++ news/serializers.py
@@ -7,3 +7,16 @@ class NewsSerializer(serializers.ModelSerializer):
     class Meta:
         model = News
         fields = '__all__'
+
+    def update(self, instance, validated_data):
+        instance.title = validated_data.get('title', instance.title)
+        instance.main_image = validated_data.get('main_image', instance.main_image)
+        instance.preview_image = validated_data.get('preview_image', instance.preview_image)
+        instance.text = validated_data.get('text', instance.text)
+        instance.publication_date = validated_data.get('publication_date', instance.publication_date)
+        instance.author = validated_data.get('author', instance.author)
+        instance.save()
+        return instance
+
+    def delete(self, instance):
+        instance.delete()
--- news/views.py
+++ news/views.py
@@ -1,4 +1,5 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.response import Response
 
 from news.models import News
 from news.serializers import NewsSerializer
@@ -7,3 +8,15 @@ from news.serializers import NewsSerializer
 class NewsViewSet(viewsets.ModelViewSet):
     queryset = News.objects.all()
     serializer_class = NewsSerializer
+
+    def update(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+        return Response(serializer.data)
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        self.perform_destroy(instance)
+        return Response(status=status.HTTP_204_NO_CONTENT)
