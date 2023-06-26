2427626532886219fab1f287284bf520772ec0bb
DanilMirosh <miroshnichenko.da.vi@mail.ru>
2023-06-26 21:29:53 +0500

    add view

---
+++ news/views.py
@@ -0,0 +1,9 @@
+from rest_framework import viewsets
+
+from news.models import News
+from news.serializers import NewsSerializer
+
+
+class NewsViewSet(viewsets.ModelViewSet):
+    queryset = News.objects.all()
+    serializer_class = NewsSerializer
