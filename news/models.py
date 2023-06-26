3ca76379cac49555adba617adf81feeebeac253f
DanilMirosh <miroshnichenko.da.vi@mail.ru>
2023-06-26 21:08:40 +0500

    add models

---
+++ news/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+# from django.contrib.gis.db import PointField
+
+
+class News(models.Model):
+    title = models.CharField(max_length=200)
+    main_image = models.ImageField(upload_to='new_images')
+    preview_image = models.ImageField(upload_to='new_images/previews')
+    text = models.TextField()
+    publication_date = models.DateField()
+    author = models.CharField(max_length=100)
+
+
+class Place(models.Model):
+    name = models.CharField(max_length=200)
+    coordinates = models.PointField()
+    rating = models.IntegerField()
