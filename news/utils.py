8ec98204675f11f447275bc65bf42a0b5c485026
DanilMirosh <miroshnichenko.da.vi@mail.ru>
2023-06-27 09:11:47 +0500

    add export_weather

--- news/utils.py
+++ news/utils.py
@@ -1,6 +1,7 @@
 import openpyxl
+import xlsxwriter
 
-from news.models import Place
+from news.models import Place, WeatherSummary
 
 
 def import_places_from_xlsx(file_path):
@@ -15,3 +16,26 @@ def import_places_from_xlsx(file_path):
         # Создайте новое место и сохраните его в базе данных
         place = Place(name=name, coordinates=coordinates, rating=rating)
         place.save()
+
+
+def export_weather_summary_to_xlsx(file_path, place_id, start_date, end_date):
+    workbook = xlsxwriter.Workbook(file_path)
+    worksheet = workbook.add_worksheet()
+
+    # Здесь вы можете настроить форматирование и структуру таблицы
+
+    # Получите сводку погоды согласно фильтру
+    weather_summaries = WeatherSummary.objects.filter(place_id=place_id, date__range=(start_date, end_date))
+
+    row = 0
+    for weather_summary in weather_summaries:
+        # Здесь вы можете заполнить таблицу данными из сводки погоды
+        worksheet.write(row, 0, weather_summary.date)
+        worksheet.write(row, 1, weather_summary.temperature)
+        worksheet.write(row, 2, weather_summary.humidity)
+        worksheet.write(row, 3, weather_summary.pressure)
+        worksheet.write(row, 4, weather_summary.wind_direction)
+        worksheet.write(row, 5, weather_summary.wind_speed)
+        row += 1
+
+    workbook.close()
