ee3b46ee80d7fff3ae99923c1bffc7467dcf0d13
DanilMirosh <miroshnichenko.da.vi@mail.ru>
2023-06-27 07:11:29 +0500

    fix url

--- news_app/urls.py
+++ news_app/urls.py
@@ -1,6 +1,10 @@
+from django.conf.urls.static import static
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
+from django_summernote import urls as summernote_urls
 from rest_framework.routers import DefaultRouter
+
 from news.views import NewsViewSet
 
 router = DefaultRouter()
@@ -9,4 +13,5 @@ router.register(r'news', NewsViewSet, basename='news')
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
-]
+    path('summernote/', include(summernote_urls)),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
